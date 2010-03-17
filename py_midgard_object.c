c09a7fa34b968c0a4d11c17abbd6460bdac1fa96
Piotr Pokora <piotrek.pokora@gmail.com>
2010-03-17 11:47:38 +0000

    Typo fix.

--- py_midgard_object.c
+++ py_midgard_object.c
@@ -617,7 +617,7 @@ _py_midgard_get_object_attribute(PyObject *self, PyObject *attr)
 	g_object_get_property(object, attr_name, &pval);
 
 	PyObject *pvalue = py_midgard_pyobject_from_gvalue((const GValue*) &pval, FALSE);
-	if (!G_VALUE_HOLDS_OBJECT (&val))
+	if (!G_VALUE_HOLDS_OBJECT (&pval))
 		g_value_unset(&pval);
 
 	return pvalue;
